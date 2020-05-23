122a7b4119d326aa9d54a2cb3f7af15e4ae9ab59
adrian9506 <38496967+adrian9506@users.noreply.github.com>
2020-05-23 23:21:59 +0200

    Przygotowywanie danych wejściowych

--- DataSmoothing/DataSmoothing/Forms/AddSeries/SeriesAddForm.cs
+++ DataSmoothing/DataSmoothing/Forms/AddSeries/SeriesAddForm.cs
@@ -27,15 +27,45 @@ namespace DataSmoothing.Forms.AddSeries
             this.delimiterComboBox.Items.Add(";");
             this.dataGridView.ColumnHeaderMouseClick += DataGridView_ColumnHeaderMouseClick;
             this.dataGridView.CellMouseClick += DataGridView_CellMouseClick;
-            this.dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            this.delimiterComboBox.SelectedIndex = 1;
         }
 
+        /// <summary>
+        /// Kliknięcie na komórkę tabeli
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void DataGridView_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
         {
             if (e.Button == MouseButtons.Right)
             {
+                ContextMenuStrip cellMenu = new ContextMenuStrip();
+                int position = e.RowIndex;
 
+                if (position >= 0)
+                {
+                    cellMenu.Items.Add("Ustaw jako nazwy kolumn").Name = "CollName";
+                    cellMenu.Items["CollName"].Tag = e.RowIndex;
+                    cellMenu.Items.Add("Usuń wiersz").Name = "RemRow";
+                    cellMenu.Items["RemRow"].Tag = e.RowIndex;
+                }
+
+                cellMenu.ItemClicked += CellMenu_ItemClicked;
+                cellMenu.Show(this.dataGridView, new Point(e.X, e.Y));
+            }
+        }
+
+        private void CellMenu_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            switch (e.ClickedItem.Name)
+            {
+                case "CollName":
+                    this.NameVariable(Convert.ToInt32(e.ClickedItem.Tag.ToString()), this._dataFiles.Rows[Convert.ToInt32(e.ClickedItem.Tag.ToString())]);
+                    break;
+                case "RemRow":
+                    this.dataGridView.Rows.RemoveAt(Convert.ToInt32(e.ClickedItem.Tag.ToString()));
+                    break;
             }
         }
 
@@ -100,22 +130,74 @@ namespace DataSmoothing.Forms.AddSeries
         /// </summary>
         /// <param name="columnIndex">Index kolumny</param>
         /// <param name="variableName">Nazwa zmiennej</param>
-        private void NameVariable(int columnIndex = -1, string variableName = "")
+        private void NameVariable(int columnIndex = -1, string variableName = "", DataRow row = null)
         {
             if (columnIndex > -1 || variableName != "")
             {
-                if (columnIndex > -1)
+                ColumnNameEditor columnNameEditor = new ColumnNameEditor("Nadaj nazwę kolumnie");
+                columnNameEditor.ShowDialog();
+                
+                foreach (ListViewItem item in this.varListView.Items)
                 {
-                    ColumnNameEditor columnNameEditor = new ColumnNameEditor("Nadaj nazwę kolumnie");
-                    columnNameEditor.ShowDialog();
-                    
-                    foreach (ListViewItem item in this.varListView.Items)
+                    if (item.Text == this._dataFiles.Columns[columnIndex].ColumnName)
                     {
-                        if (item.Text == this._dataFiles.Columns[columnIndex].ColumnName)
+                        this.varListView.Items[this.varListView.Items.IndexOf(item)].Text = columnNameEditor.GetName;
+                        this._dataFiles.Columns[columnIndex].ColumnName = columnNameEditor.GetName;
+                        break;
+                    }
+                }
+            }
+
+            if (row != null)
+            {
+                foreach (ListViewItem item in this.varListView.Items)
+                {
+                    for (int i = 0; i < this.dataGridView.Columns.Count; i++)
+                    {
+                        if (item.Text == this._dataFiles.Columns[i].ColumnName)
                         {
-                            this.varListView.Items[this.varListView.Items.IndexOf(item)].Text = columnNameEditor.GetName;
+                            this.varListView.Items[this.varListView.Items.IndexOf(item)].Text = row[i].ToString();
+                            this._dataFiles.Columns[columnIndex].ColumnName = row[i].ToString();
                             break;
                         }
+                    }
+                }
+            }
+        }
+
+        private void NameVariable(int rowIndex, DataRow row)
+        {
+            if (row != null)
+            {
+                string[] _names = new string[this._dataFiles.Columns.Count];
+
+                for (int i = 0; i < this._dataFiles.Columns.Count; i++)
+                {
+                    _names[i] = row[i].ToString();
+                }
+
+                this._dataFiles.Rows.RemoveAt(rowIndex);
+                foreach (ListViewItem item in this.varListView.Items)
+                {
+                    for (int i = 0; i < this.dataGridView.Columns.Count; i++)
+                    {
+                        if (item.Text == this._dataFiles.Columns[i].ColumnName)
+                        {
+                            this.varListView.Items[this.varListView.Items.IndexOf(item)].Text = _names[i];
+                            break;
+                        }
+                    }
+                }
+
+                for (int i = 0; i < this.dataGridView.Columns.Count; i++)
+                {
+                    try
+                    {
+                        this._dataFiles.Columns[i].ColumnName = _names[i];
+                    }
+                    catch (Exception e)
+                    {
+                        this._dataFiles.Columns[i].ColumnName = _names[i] + "2";
                     }
                 }
             }
@@ -201,6 +283,16 @@ namespace DataSmoothing.Forms.AddSeries
                                 for (int i = 0; i < _firstLine.Count(); i++)
                                 {
                                     this._dataFiles.Columns.Add(_firstLine[i].ToString());
+
+                                    if (this._dataFiles.Columns[_firstLine[i].ToString()] != null)
+                                    {
+                                        this._dataFiles.Columns[_firstLine[i].ToString()].ReadOnly = true;
+                                    }
+                                }
+
+                                if (this._dataFiles.Columns.Count <= 4)
+                                {
+                                    this.dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
                                 }
 
                                 while (!sr.EndOfStream)
@@ -247,9 +339,12 @@ namespace DataSmoothing.Forms.AddSeries
         /// <param name="e"></param>
         private void delimiterComboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
-            this._dataFiles.Clear();
-            this._dataFiles.Columns.Clear();
-            this.PrepareData(this._filePath.Last());
+            if (_filePath.Count > 0)
+            {
+                this._dataFiles.Clear();
+                this._dataFiles.Columns.Clear();
+                this.PrepareData(this._filePath.Last());
+            }
         }
     }
 }
